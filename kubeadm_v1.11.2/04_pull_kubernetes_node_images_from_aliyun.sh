19b59068bf166e3cfd210ce79555bb974eebf533
zhongjinfeng <441692003@qq.com>
2018-08-30 01:14:06 +0800

    fix bug

--- kubeadm_v1.11.2/04_pull_kubernetes_node_images_from_aliyun.sh
+++ kubeadm_v1.11.2/04_pull_kubernetes_node_images_from_aliyun.sh
@@ -7,9 +7,7 @@ set -e
 # For running kubeadm without an internet connection you have to pre-pull the required master images for the version of choice:
 KUBE_VERSION=v1.11.2
 KUBE_PAUSE_VERSION=3.1
-
-GCR_URL=k8s.gcr.io
-ALIYUN_URL=anjia0532
+username=anjia0532
 
 # When test v1.11.0, I found Kubernetes depends on both pause-amd64:3.1 and pause:3.1 
 
